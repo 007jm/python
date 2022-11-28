fe80b2726f1f44c06129eb29c36e718278e14317
007jm <112930591+007jm@users.noreply.github.com>
2022-11-28 22:18:40 +0900

    007

---
+++ 002.py
@@ -0,0 +1,29 @@
+## 1
+# i=input("숫자 입력:")
+# p=input("문자 입력:")
+# print(i+p)
+
+## 2
+# print("과천코딩2022"[0:6])
+
+## 3
+# a = input("과일 이름 : ")
+# b = input("과일 갯수 : ")
+# print(f"{a}과일을 {b}개만큼 먹었다")
+
+## 4
+# j=int(input("숫자 입력1 : "))
+# m=int(input("숫자 입력2 : "))
+# print(j+m)
+# print(j-m)
+# print(j*m)
+# print(j/m)
+
+## 5
+# f =  int(input("숫자 입력 3 : "))
+# if f%2==0:
+#     print("짝수")
+# elif f%2==1:
+#     print("홀수")
+
+## 6
