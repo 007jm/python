f2606b47890d1d2f934fd939e569ad44c48a83c6
007jm <112930591+007jm@users.noreply.github.com>
2022-11-29 16:40:05 +0900

    Add files via upload

--- class12.py
+++ class12.py
@@ -1,4 +1,84 @@
 ###중간평가
-#1. 
-a= "과천코딩2022"
-print(a[0:4])
+
+## 1
+# i=input("숫자 입력:")
+# p=input("문자 입력:")
+# print(i+p)
+
+## 2
+# print("과천코딩2022"[2:6])
+
+## 3
+# a = input("과일 이름 : ")
+# b = input("과일 갯수 : ")
+# print(f"{a}과일을 {b}개만큼 먹었다")
+
+## 4
+# j=int(input("숫자 입력1 : "))
+# m=int(input("숫자 입력2 : "))
+# print(j+m)
+# print(j-m)
+# print(j*m)
+# print(j/m)
+
+## 5
+# f =  int(input("숫자 입력 3 : "))
+# if f%2==0:
+#     print("짝수")
+# elif f%2==1:
+#     print("홀수")
+
+## 6
+# count= 1
+# c = int(input("숫자입력: "))
+# while count<=c:
+#     if c%count==0:
+#         print(count)
+#     count+=1
+
+#for문
+    
+## 7
+# d = 0
+# count=1
+# c = int(input("숫자 입력 : "))
+# while count<=c:
+#     if c%count==0:
+#         print(count)
+#         d+=1
+#     count+=1
+# if d >2:
+#     print("false")
+# else:
+#     print("true")
+
+#for 문
+## 8
+# count=1
+# g = int(input("구구단 숫자 입력: "))
+# while count<10:
+#     print(f"{g}*{count}={g*count}")
+#     count+=1
+
+#for문
+## 9
+# c =1
+# d = 0
+# h=int(input("숫자 입력: "))
+# while h>=c:
+#     d+=(1+c)
+#     c+=1
+# print(d-h)
+
+
+## 10
+w= int(input("숫자입력:"))
+c =0
+while c<=w:
+    print (c)
+    c+=1
+    
+for i in range(0,10,3):
+    print("x")
+
+if ##나머지 연산중요
