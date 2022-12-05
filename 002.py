45f4fbece56e99c11eef016617bbbf42e13f3139
007jm <112930591+007jm@users.noreply.github.com>
2022-12-05 22:08:22 +0900

    Add files via upload

--- 002.py
+++ 002.py
@@ -26,4 +26,39 @@
 # elif f%2==1:
 #     print("홀수")
 
-## 6
+# 10
+j = 0
+c = 0
+m = int(input("입력: "))
+
+while c<=m:
+    if c==3:
+        print("*")
+        c+=1
+        j+=1
+    elif c==6:
+        print("*")
+        c+=1
+    elif c==9:
+        print("*")
+        c+=1
+    else:
+        print(c)
+    c+=1
+    j+=1
+
+
+
+
+
+
+
+
+
+# for i in range(0,100,):
+#     i = i.split(3)
+
+# c=0
+# while True :
+#     print(123456789123456789).split(3)
+#     c+=1
