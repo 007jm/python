326fef331105c24bf28c433628a3f2cf0ffd6090
007jm <112930591+007jm@users.noreply.github.com>
2022-12-06 16:41:58 +0900

    Add files via upload

--- class12.py
+++ class12.py
@@ -61,7 +61,8 @@
 #     count+=1
 
 #for문
-## 9
+# 9
+#while시도
 # c =1
 # d = 0
 # h=int(input("숫자 입력: "))
@@ -70,15 +71,70 @@
 #     c+=1
 # print(d-h)
 
+# 9
+#range시도
+# c=0
+# h=int(input("dlqfur: "))
+# for i in range(1,h+1):
+#     c+=i
+# print(c)
 
-## 10
-w= int(input("숫자입력:"))
-c =0
-while c<=w:
-    print (c)
-    c+=1
+
+
+# ## 10
+# w= int(input("숫자입력:"))
+# c =0
+# while c<=w:
+#     print (c)
+#     c+=1
     
-for i in range(0,10,3):
-    print("x")
+# for i in range(0,10,3):
+#     print("x")
 
-if ##나머지 연산중요
+# if ##나머지 연산중요
+
+# 10
+# j = 0
+# c = 0
+# m = int(input("입력: "))
+# while j<=m:
+#     while c<=10:
+#         if c==3:
+#             print("*")
+#             c+=1
+#             j+=1
+#         elif c==6:
+#             print("*")
+#             c+=1
+#             j+=1
+#         elif c==9:
+#             print("*")
+#             c+=1
+#             j+=1
+#         else:
+#             print(c)
+#             c+=1
+#             j+=1
+#         c-=10
+        
+        
+        
+##3,6,9
+# num = int(input("숫자입력: "))
+
+# for i in range(1, num+1):
+#     if i % 10 == 3 or i%10==6 or i %10==9:
+#         print("X")
+#     elif i // 10==3 or i// 10 == 6 or i //10== 9 :
+#         print("X")
+#     else:
+#         print(i)
+
+
+##두가지 주사위
+k=[]
+for i in range(1,7):
+    for g in range(1,7):
+        print(i,g)
+        
+###
