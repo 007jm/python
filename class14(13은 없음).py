c2cca8a58a6433be0de3d6e288c7c1c8c58282fa
007jm <112930591+007jm@users.noreply.github.com>
2022-12-06 16:42:17 +0900

    Add files via upload

---
+++ class14(13은 없음).py
@@ -0,0 +1,14 @@
+###함수
+##외장함수
+
+import random
+import time
+print(random.randint(1,100))
+print(time.ctime())
+print(time.localtime)
+
+while True:
+    import time
+    print(time.sleep(1))
+    print(time.ctime())
+    
